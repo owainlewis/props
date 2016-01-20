e475024cb8673b367b6e9a1308f5fd733770ba66
Owain Lewis <owain@owainlewis.com>
2016-01-20 17:12:12 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject props "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Small library for working with java.util.Properties"
+  :url "http://github.com/owainlewis/props"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
